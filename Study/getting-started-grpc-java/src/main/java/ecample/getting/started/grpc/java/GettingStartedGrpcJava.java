6fe238f179e01a02263fca446804184a8b0138b3
Seunghye.Stella.Kim <120572485+stella3760@users.noreply.github.com>
2023-07-28 16:32:14 +0100

    studying

---
+++ Study/getting-started-grpc-java/src/main/java/ecample/getting/started/grpc/java/GettingStartedGrpcJava.java
@@ -0,0 +1,12 @@
+package ecample.getting.started.grpc.java;
+
+/**
+ *
+ * @author seunghyekim
+ */
+public class GettingStartedGrpcJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
